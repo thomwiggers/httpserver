a4f9653f20c50603cd913c8e4bf7060ec285a687
Thom Wiggers <thom@thomwiggers.nl>
2015-03-01 15:46:15 +0100

    Add echo server example

--- httpserver/__init__.py
+++ httpserver/__init__.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import, print_function
+
+import logging
 
 __author__ = 'Thom Wiggers'
 __email__ = 'thom@thomwiggers.nl'
 __version__ = '0.1.0'
 
 
+def _start_server(bindaddr, port):
+    import asyncio
+    from .httpserver import HttpProtocol
+    loop = asyncio.get_event_loop()
+    coroutine = loop.create_server(HttpProtocol, bindaddr, port)
+    server = loop.run_until_complete(coroutine)
+
+    print('Starting server on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+
 def run(argv=None):  # pragma: no cover
     """Run the HTTP server
 
-    Usage: httpserver [options]
+    Usage: httpserver [options] [<bindaddress>] [<port>]
+
+    Default bind address: 127.0.0.1
+    Default port:         8080
 
     Options::
 
@@ -21,4 +41,16 @@ def run(argv=None):  # pragma: no cover
     argv = argv or sys.argv[1:]
     args = docopt.docopt(textwrap.dedent(run.__doc__), argv)
 
-    print("Bla")
+    level = logging.WARNING
+    if args['--verbose']:
+        level = logging.INFO
+    if args['--debug']:
+        level = logging.DEBUG
+    logging.basicConfig(level=level)
+
+    logger = logging.getLogger('run method')
+    logger.debug('CLI args: %s' % args)
+
+    bindaddr = args['<bindaddress>'] or '127.0.0.1'
+    port = args['<port>'] or '8080'
+    _start_server(bindaddr, port)
--- httpserver/httpserver.py
+++ httpserver/httpserver.py
@@ -1,1 +1,26 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class HttpProtocol(asyncio.Protocol):
+    """HTTP/1.1 Protocol implementation
+
+    Per connection made, one of these gets instantiated
+    """
+
+    def __init__(self):
+        self.logger = logger.getChild('HttpProtocol {}'.format(id(self)))
+        self.logger.debug('Instantiated HttpProtocol')
+
+    def connection_made(self, transport):
+        self.logger.info("Connection made at object %s", id(self))
+        self.transport = transport
+
+    def data_received(self, data):
+        self.logger.info("Received data: %s", repr(data))
+        self.transport.write("Hi I see your %s", data)
